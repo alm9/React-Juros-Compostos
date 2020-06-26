e8dd5ceda78ec9bbc3bbc9f269859f9b6aa61ab1
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-26 11:28:47 -0300

    starting the development of compound interest calculation

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-Juros-Compostos
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cálculo de Juros Compostos</h1>
+        <h2>
+          By <code>André Machado</code> using <code>React Hooks</code>
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
